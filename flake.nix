9537b8d86950d2d1ffef197b676d9994c8a78731
grig-iv <abstractgrig@gmail.com>
2024-11-13 00:37:47 +0300

    add flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "My prompt flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        packages.default = pkgs.buildGoModule {
+          name = "plato";
+          src = ./.;
+          vendorHash = "sha256-qf787Dg6Xy3HGkgroATQ+Xd15muLpkVDuzFskBktEXo=";
+        };
+      }
+    );
+}
